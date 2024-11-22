dc36911f04e65017b1404c9cf77c316b423df54c
Nick Lackman <lackmannicholas@gmail.com>
2024-11-21 21:02:55 -0800

    simplifying api for deployment testing

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,30 +1,44 @@
-#[macro_use]
-extern crate diesel;
-#[macro_use]
-extern crate diesel_migrations;
+/*An actix Microservice that has multiple routes:
+A.  / that turns a hello world
+B. /fruit that returns a random fruit
+C. /health that returns a 200 status code
+D. /version that returns the version of the service
+*/
 
-use actix_web::{App, HttpServer};
-use listenfd::ListenFd;
+use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use rand::seq::SliceRandom;
 use std::env;
 
-mod db;
-mod employees;
-mod error_handler;
-mod schema;
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
 
-#[actix_rt::main]
+async fn fruit() -> impl Responder {
+    let fruits = vec!["Apple", "Banana", "Cherry", "Date", "Elderberry", "Fig", "Grape", "Honeydew", "Jackfruit", "Kiwi", "Lemon", "Mango", "Nectarine", "Orange", "Papaya", "Quince", "Raspberry", "Strawberry", "Tangerine", "Ugli", "Vineyard Peach", "Watermelon", "Xigua", "Yellow Passion Fruit", "Zucchini"];
+    let fruit = fruits.choose(&mut rand::thread_rng()).unwrap();
+    // return fruit as repsonse body
+    HttpResponse::Ok().body(*fruit)
+}
+
+async fn health() -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+async fn version() -> impl Responder {
+    let version = env!("CARGO_PKG_VERSION");
+    HttpResponse::Ok().body(version)
+}
+
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    dotenv::dotenv().ok();
-    db::init();
-    let mut listenfd = ListenFd::from_env();
-    let mut server = HttpServer::new(|| App::new().configure(employees::init_routes));
-    server = match listenfd.take_tcp_listener(0)? {
-        Some(listener) => server.listen(listener)?,
-        None => {
-            let host = env::var("HOST").expect("Please set host in .env");
-            let port = env::var("PORT").expect("Please set port in .env");
-            server.bind(format!("{}:{}", host, port))?
-        }
-    };
-    server.run().await
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(hello))
+            .route("/fruit", web::get().to(fruit))
+            .route("/health", web::get().to(health))
+            .route("/version", web::get().to(version))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
