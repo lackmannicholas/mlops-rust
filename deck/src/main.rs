d2e535ba4b3081cded7555865801b038380e1fd0
Nick Lackman <lackmannicholas@gmail.com>
2024-11-24 08:43:29 -0800

    creating new impl Deck

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -3,28 +3,33 @@ struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    // array of suits for a deck of cards
-    let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
+impl Deck {
+    fn new() -> Self {
+        // array of suits for a deck of cards
+        let suits = ["Hearts", "Diamonds", "Clubs", "Spades"];
 
-    // array of ranks for a deck of cards
-    let ranks = [
-        "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King",
-    ];
+        // array of ranks for a deck of cards
+        let ranks = [
+            "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King",
+        ];
 
-    // create a new cards vector of strings
-    let mut cards: Vec<String> = Vec::new();
+        // create a new cards vector of strings
+        let mut cards: Vec<String> = Vec::new();
 
-    // double for loop to create a deck of cards
-    for suit in suits {
-        for rank in ranks {
-            // add to suit and rank combination to the deck
-            cards.push(format!("{} of {}", rank, suit));
+        // double for loop to create a deck of cards
+        for suit in suits {
+            for rank in ranks {
+                // add to suit and rank combination to the deck
+                cards.push(format!("{} of {}", rank, suit));
+            }
         }
+
+        Deck { cards }
     }
+}
 
-    let deck = Deck { cards };
+fn main() {
+    let deck = Deck::new();
 
-
-    println!("Here's your deck: {:?}", deck);
+    println!("Here's your deck: {:#?}", deck);
 }
